76d725151ab901ba1e40d17b49eb7a9833554bf2
JoaoPedroAbreu <jaohds@gmail.com>
2025-07-11 17:31:23 -0300

    feat: add products routes and controller, as well as product fields validations

---
+++ app/backend/src/products/dtos/create-product.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsOptional, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+}
--- app/backend/src/products/products.service.ts
+++ app/backend/src/products/products.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateProductDto) {
+    return this.prisma.product.create({ data });
+  }
+
+  async findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.product.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: UpdateProductDto) {
+    await this.findOne(id); // lança erro se não existir
+    return this.prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id); // lança erro se não existir
+    return this.prisma.product.delete({ where: { id } });
+  }
+}
